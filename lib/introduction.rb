e45f281dee86be21469519d425c79d46f11e34e9
Jared Harbison <jared.harbison@gmail.com>
2018-12-18 19:40:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,8 @@ end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} "
+  
+  
+  def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
